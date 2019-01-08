865429b3578d19b69828d98e473c5c7df006d69d
Corey Godine <corey.godine@gmail.com>
2019-01-08 16:12:53 -0600

    First version of an app I made

---
+++ onerepmax.py
@@ -0,0 +1,19 @@
+# Script that will take inputs of weight (pounds or kilograms) and amount of
+# reps performed and output an estimated one rep max based on an average of three
+# formulas
+
+
+weight_lifted=float(input("Enter amount of weight lifted (in either pounds or kilograms): "))
+while weight_lifted<0:
+    weight_lifted=float(input("Weight must be greater than zero. Enter a weight greater than zero: "))
+    
+exercise_reps=float(input("Enter amount of repetitions performed: "))
+while exercise_reps<0:
+    exercise_reps=float(input("Reps must be greater than zero. Enter amount of reps greater than zero: "))
+
+brzycki=weight_lifted*(36/(37-exercise_reps))
+lombardi=weight_lifted*exercise_reps**0.1
+wendler=weight_lifted*exercise_reps*0.0333+weight_lifted
+
+one_rep_max=(brzycki+lombardi+wendler)/3
+print("Your one rep max estimate is: "+str(one_rep_max)+" pounds")
