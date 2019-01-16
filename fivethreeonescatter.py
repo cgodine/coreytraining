2b932c775cc3c38961238760c3fc20a3e9d7d493
Corey Godine <corey.godine@gmail.com>
2019-01-16 14:10:14 -0600

    Plotted data in scatter plot

---
+++ fivethreeonescatter.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Import csv data
+FTO=pd.read_csv('/Users/cgodine/Desktop/gapp/fivemaxest.csv')
+
+# Set axes labels, Plot Data
+x=np.arange(10)
+plt.xlim([0,10])
+plt.ylim([100, 700])
+plt.xlabel('Cycle')
+plt.ylabel('Pounds')
+
+
+plt.scatter(x, FTO.iloc[:,1], color='blue') # Squat
+plt.scatter(x, FTO.iloc[:,2], color='red') # Bench
+plt.scatter(x, FTO.iloc[:,3], color='green') # Deadlift
+
+# Legend
+plt.legend(['Squat', 'Bench Press', 'Deadlift'], loc='lower right')
+# Show Plot
+plt.savefig('2018scatter.png')
