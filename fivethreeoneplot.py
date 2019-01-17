b5645a51b4b021b4d08a545abb1f2e3b6e3a726c
Corey Godine <corey.godine@gmail.com>
2019-01-17 13:33:10 -0600

    added expanded dataset with all 30 weeks of 1rm

--- fivethreeoneplot.py
+++ fivethreeoneplot.py
@@ -3,21 +3,42 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Import csv data
-FTO=pd.read_csv('/Users/cgodine/Desktop/gapp/fivemaxest.csv')
+FTO=pd.read_csv('/Users/cgodine/Desktop/gapp/fivethreeone_2018.csv')
 
-# Set axes labels, Plot Data
+# Set axes limits, labels, grid
 x=np.arange(10)
 plt.xlim([0,10])
 plt.ylim([100, 700])
 plt.xlabel('Cycle')
 plt.ylabel('Pounds')
+plt.minorticks_on()
+plt.grid(which='major', axis='y', linestyle='-')
+plt.grid(which='minor', axis='y', linestyle=':')
 
+# Chart title
+plt.title('Corey\'s 2018 5/3/1 Progress')
 
-plt.plot(x, FTO.iloc[:,1], color='blue') # Squat
-plt.plot(x, FTO.iloc[:,2], color='red') # Bench
-plt.plot(x, FTO.iloc[:,3], color='green') # Deadlift
+# Plot data for 5 wave
+plt.plot(x, FTO.loc[:,'Squat'], color='blue', label='5 Sq') # Squat
+plt.plot(x, FTO.loc[:,'Bench'], color='red', label='5 Be') # Bench
+plt.plot(x, FTO.loc[:,'Deadlift'], color='green', label='5 DL') # Deadlift
+
+# Plot 3 wave data
+plt.plot(x, FTO.loc[:, 'Squat.1'], color='cyan', label='3 Sq') # Squat
+plt.plot(x, FTO.loc[:, 'Bench.1'], color='salmon', label='3 Be') # Bench
+plt.plot(x, FTO.loc[:, 'Deadlift.1'], color='lime', label='3 DL') # Deadlift
+
+# Plot 531 wave
+plt.plot(x, FTO.loc[:, 'Squat.2'], color='darkblue', label='Max Sq') # Squat
+plt.plot(x, FTO.loc[:, 'Bench.2'], color='crimson', label='Max Be') # Bench
+plt.plot(x, FTO.loc[:, 'Deadlift.2'], color='darkgreen', label='Max DL') # Deadlift
 
 # Legend
-plt.legend(['Squat', 'Bench Press', 'Deadlift'], loc='lower right')
+plt.legend(
+    ['Sq', 'Be', 'DL', '3 Sq', '3 Be', '3 DL','Max Sq', 'Max Be', 'Max DL'], 
+    loc='lower right', prop={'size': 7.5}
+    )
 # Show Plot
-plt.savefig('2018.png')
+# plt.show()
+
+plt.savefig('2018plot.png')
--- fivethreeonescatter.py
+++ fivethreeonescatter.py
@@ -7,24 +7,43 @@ from matplotlib import pylab
 from scipy import stats
 
 # Import csv data
-FTO=pd.read_csv('/Users/cgodine/Desktop/gapp/fivemaxest.csv')
+FTO=pd.read_csv('/Users/cgodine/Desktop/gapp/fivethreeone_2018.csv')
 
-# Set axes labels, Plot Data
+# Set axes limits, labels, grid
 x=np.arange(10)
 plt.xlim([0,10])
 plt.ylim([100, 700])
 plt.xlabel('Cycle')
 plt.ylabel('Pounds')
+plt.minorticks_on()
+plt.grid(which='major', axis='y', linestyle='-')
+plt.grid(which='minor', axis='y', linestyle=':')
 
-# Scatter Plots
-plt.scatter(x, FTO.iloc[:,1], color='blue') # Squat
-plt.scatter(x, FTO.iloc[:,2], color='red') # Bench
-plt.scatter(x, FTO.iloc[:,3], color='green') # Deadlift
+# Chart title
+plt.title('Corey\'s 2018 5/3/1 Progress')
+
+# Plot data for 5 wave
+plt.scatter(x, FTO.loc[:,'Squat'], color='blue', label='5 Sq') # Squat
+plt.scatter(x, FTO.loc[:,'Bench'], color='red', label='5 Be') # Bench
+plt.scatter(x, FTO.loc[:,'Deadlift'], color='green', label='5 DL') # Deadlift
+
+# Plot 3 wave data
+plt.scatter(x, FTO.loc[:, 'Squat.1'], color='cyan', label='3 Sq') # Squat
+plt.scatter(x, FTO.loc[:, 'Bench.1'], color='salmon', label='3 Be') # Bench
+plt.scatter(x, FTO.loc[:, 'Deadlift.1'], color='lime', label='3 DL') # Deadlift
+
+# Plot 531 wave
+plt.scatter(x, FTO.loc[:, 'Squat.2'], color='darkblue', label='Max Sq') # Squat
+plt.scatter(x, FTO.loc[:, 'Bench.2'], color='crimson', label='Max Be') # Bench
+plt.scatter(x, FTO.loc[:, 'Deadlift.2'], color='darkgreen', label='Max DL') # Deadlift
 
 # Legend
-plt.legend(['Squat', 'Bench Press', 'Deadlift'], loc='lower right')
+plt.legend(
+    ['Sq', 'Be', 'DL', '3 Sq', '3 Be', '3 DL','Max Sq', 'Max Be', 'Max DL'], 
+    loc='lower right', prop={'size': 7.5}
+    )
 
-# Linear Regressions
+"""# Linear Regressions
 # Squat
 slope1, intercept1, r_value1, p_value1, std_err1 = stats.linregress(x, FTO.iloc[:,1])
 line1=slope1*x+intercept1
@@ -38,7 +57,7 @@ plt.plot(line2)
 # Deadlift 
 slope3, intercept3, r_value3, p_value3, std_err3 = stats.linregress(x, FTO.iloc[:,3])
 line3=slope3*x+intercept3
-plt.plot(line3)
+plt.plot(line3)"""
 
 # plt.show()
 
