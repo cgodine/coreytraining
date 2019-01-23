28637b33ca8c39691981b01dcef3edcf79584b37
Corey Godine <corey.godine@gmail.com>
2019-01-23 10:17:58 -0600

    better organized code into function

--- onerepmax.py
+++ onerepmax.py
@@ -3,17 +3,21 @@
 # formulas
 
 
-weight_lifted=float(input("Enter amount of weight lifted (in either pounds or kilograms): "))
-while weight_lifted<0:
-    weight_lifted=float(input("Weight must be greater than zero. Enter a weight greater than zero: "))
+
+def calculate_one_rep_max(weight, reps):
+    weight_lifted=float(weight)
+    while weight_lifted<0:
+        weight_lifted=float(input("Weight must be greater than zero. Enter a weight greater than zero: "))
     
-exercise_reps=float(input("Enter amount of repetitions performed: "))
-while exercise_reps<0:
-    exercise_reps=float(input("Reps must be greater than zero. Enter amount of reps greater than zero: "))
+    total_reps=float(reps)
+    while total_reps<0:
+        total_reps=float(input("Reps must be greater than zero. Enter amount of reps greater than zero: "))
 
-brzycki=weight_lifted*(36/(37-exercise_reps))
-lombardi=weight_lifted*exercise_reps**0.1
-wendler=weight_lifted*exercise_reps*0.0333+weight_lifted
+    brzycki=weight_lifted*(36/(37-total_reps))
+    lombardi=weight_lifted*total_reps**0.1
+    wendler=weight_lifted*total_reps*0.0333+weight_lifted
 
-one_rep_max=int((brzycki+lombardi+wendler)/3)
-print("Your one rep max estimate is: "+str(one_rep_max)+" pounds")
+    one_rep_max=int((brzycki+lombardi+wendler)/3)
+    print("Your one rep max estimate is: "+str(one_rep_max)+" pounds")
+
+calculate_one_rep_max(input("Enter weight in pounds or kilograms: "), input("Enter amount of reps performed: "))
