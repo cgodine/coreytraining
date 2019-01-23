126738c1b8dbade4655a74481927db1e0d44e7fe
Corey Godine <corey.godine@gmail.com>
2019-01-23 14:14:16 -0600

    consolidated output print statements

--- onerepmax.py
+++ onerepmax.py
@@ -13,12 +13,19 @@ def calculate_one_rep_max(weight, reps):
     while total_reps<0 or total_reps > 10:
         total_reps=float(input("Reps must be greater than zero or less than 10. Enter amount of reps between zero and 10: "))
 
+
     brzycki=weight_lifted*(36/(37-total_reps))
     lombardi=weight_lifted*total_reps**0.1
     wendler=weight_lifted*total_reps*0.0333+weight_lifted
 
-    one_rep_max=int((brzycki+lombardi+wendler)/3)
-    print("Your one rep max estimate is: "+str(one_rep_max)+" pounds")
-    return one_rep_max
+    if total_reps == 1:
+        one_rep_max=int(weight_lifted)
+        return one_rep_max
 
-calculate_one_rep_max(input("Enter weight in pounds or kilograms: "), input("Enter amount of reps performed: "))
+    else:
+        one_rep_max=int((brzycki+lombardi+wendler)/3)
+        return one_rep_max
+
+
+# output = calculate_one_rep_max(input("Enter weight in pounds or kilograms: "), input("Enter amount of reps performed: "))
+# print("Your one rep max estimate is: "+str(output))
